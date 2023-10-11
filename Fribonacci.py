2e71f8f37a92eb0acab4384b852636ca2e3d3b13
Alrrep <perlaa.rmn@gmail.com>
2023-10-11 13:26:39 -0600

    Grafico

--- Fribonacci.py
+++ Fribonacci.py
@@ -6,6 +6,7 @@
 #suma= 1+2+3+4+5+6+7+8+9+10
 #print(suma)
 import math
+import matplotlib.pyplot as plt
 fibo=[]
 N=100
 a=1
@@ -25,8 +26,15 @@ print(fibo)
 for i in range(len(fibo)-1):
     print(fibo[i+1]/fibo[i])
 
-gold_ration=fibo[-1]/fibo[-2]
-print(gold_ration)
+golden_ratio=fibo[-1]/fibo[-2]
+print(golden_ratio)
+
+x=[0,1,1,0,0]
+y=[0,0,golden_ratio,golden_ratio,0]
+plt.plot(x,y)
+plt.gca().set_aspect("equal")
+plt.show()
+
 
 #Razon dorada
 #print((1.0+math.sqrt(5))/2.0)
